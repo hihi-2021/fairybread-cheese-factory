2e0a50177bace6eec2ace56d42367e34abdb9d5d
raz-yusoff <razalee.ali@gmail.com>
2021-10-21 17:15:39 +1300

    initialized views

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const hbs = require('express-handlebars')
 const fs = require('fs')
-const { get } = require('http')
-const server = express()
+const multer = require('multer')
 
+
+// middleware
+server.use(express.static('public'))
+server.use(express.urlencoded({ extended: false }))
+
+
+// handlebars configuration
+server.engine('hbs', hbs({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+
+
+// routes
 server.get('/', (req,res) => {
     res.send('<h1>hello</h1><p>Bye</p>')
 })
