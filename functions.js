1c0fa8eba49d32331be72e818ead4a37f3f0c288
Olivia Schultz <Olivia@Olivias-MacBook-Air.local>
2021-10-22 12:24:17 +1300

    Homepage Styles, Add Cheese Styles, Start of Vote Functionality

---
+++ functions.js
@@ -0,0 +1,23 @@
+const cheeseData = require("cheeseData")
+
+function newVote(cheeseInfo) {
+
+   const newVoteValue =  cheeseInfo.votes + 1
+
+   const found = cheeseData.find(cheese => cheese.id === cheeseInfo.id);
+   found.votes = newVoteValue
+
+   fs.writeFile( "cheeseData.json",cheeseData, "utf8")
+
+
+}
+
+
+
+//Cheeseinfo could contain the ID and the vote value of the cheese that was voted for
+
+//When button "Vote" is clicked then the vote attribute on the cheese object should increment by 1
+
+//
+
+module.exports = newVote
