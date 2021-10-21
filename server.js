484bf209d2d8ec12292e544448a353c3502ffbc8
raz-yusoff <razalee.ali@gmail.com>
2021-10-22 11:02:55 +1300

    link to add form

--- server.js
+++ server.js
@@ -15,11 +15,18 @@ server.set('view engine', 'hbs')
 
 
 // routes
-server.get('/', (req,res) => {
-    const viewData = {
-        title: 'The Grate Cheese Off',
-      }
-      const template = 'home'
+server.get('/:id', (req,res) => {
+    if (req.params.id == "home") {
+        var viewData = {
+            title: 'The Grate Cheese Off',
+          }
+        var template = 'home'
+    } else if (req.params.id == "add") {
+        var viewData = {
+            title: 'Cheese Culture Factory',
+          }
+        var template = 'add'
+    }
       res.render(template, viewData)
 })
 
